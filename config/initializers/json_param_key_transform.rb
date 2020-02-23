bb2350532546e650e1c0840652f4012e3a854908
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-02-22 23:32:11 -0600

    auto transform params from camelcase to snakecase

---
+++ config/initializers/json_param_key_transform.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+ActionDispatch::Request.parameter_parsers[:json] = lambda { |raw_post|
+  # Modified from action_dispatch/http/parameters.rb
+  data = ActiveSupport::JSON.decode(raw_post)
+  data = { _json: data } unless data.is_a?(Hash)
+
+  # Transform camelCase param keys to snake_case:
+  data.deep_transform_keys!(&:underscore)
+}
